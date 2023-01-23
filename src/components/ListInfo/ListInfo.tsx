e3fcfc3be4877ac43df17f3bcdf112fbb3fcb895
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-01-23 13:46:41 -0300

    Updates- UsersList

--- src/components/ListInfo/ListInfo.tsx
+++ src/components/ListInfo/ListInfo.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import styles from '../ListInfo/ListInfo.module.css';
 import UserList from "../UserList/UserList";
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import { color } from "@mui/system";
+
+const options = [
+  {
+    value: 'País',
+    label: 'País',
+  },
+  {
+    value: 'Projetos',
+    label: 'Projetos',
+  },
+  {
+    value: 'Assinaturas',
+    label: 'Assinaturas',
+  },
+ 
+];
+
 
 export default function ListInfo(){
     return (
@@ -12,7 +32,19 @@ export default function ListInfo(){
             Exportar Planilha
             </button>
           </div>
-
+          <TextField id="outlined-basic" label="Nome/Id/Email"  defaultValue="" variant="outlined" style={{  borderRadius:'4px', color:'white',marginLeft:'45px',marginTop:'15px',marginBottom:'15px'}}/>
+          
+          <TextField
+          id="outlined-select-currency"
+          select
+          defaultValue="País"
+          style={{borderColor: 'white',marginLeft:'45px',marginTop:'15px',marginBottom:'15px'}}>
+           {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
 
         </div>
       );
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -27,39 +27,50 @@ const rows = [
   createData('Annete Black', '12539', 'email@email.com', 3, 2,'Brasil', '17/01/2023'),
   createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
   createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
+  createData('Annete Black', '12539', 'email@email.com', 3, 2, 'Brasil','17/01/2023'),
   
 ];
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} style={{width:'1198px', overflow:'-moz-initial'}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome do Usuário</TableCell>
-            <TableCell align="right">ID do Usuário</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Projetos</TableCell>
-            <TableCell align="right">Assinaturas</TableCell>
-            <TableCell align="right">País</TableCell>
-            <TableCell align="right">Último Login</TableCell>
+        <TableHead >
+          <TableRow style={{background: 'rgba(138, 135, 151, 0.2)', borderRadius:'40px', marginBottom:'10px',textAlign:'center'}}>
+            <TableCell style={{textAlign:'center'}}>Nome do Usuário</TableCell>
+            <TableCell align="right" style={{textAlign:'center'}}>ID do Usuário</TableCell>
+            <TableCell align="right"style={{textAlign:'center'}}>Email</TableCell>
+            <TableCell align="right"style={{textAlign:'center'}}>Projetos</TableCell>
+            <TableCell align="right"style={{textAlign:'center'}}>Assinaturas</TableCell>
+            <TableCell align="right"style={{textAlign:'center'}}>País</TableCell>
+            <TableCell align="right"style={{textAlign:'center'}}>Último Login</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
+        
+        <TableBody style={{textAlign:'center'}} >
           {rows.map((row) => (
+              
             <TableRow
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              style={{background:'#1B2232', borderRadius:'10px'}}
             >
-              <TableCell component="th" scope="row">
+              <TableCell component="th" scope="row"  style={{ color:'white',textAlign:'center'}}>
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.id}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.projetos}</TableCell>
-              <TableCell align="right">{row.assinaturas}</TableCell>
-              <TableCell align="right">{row.pais}</TableCell>
-              <TableCell align="right">{row.lastLogin}</TableCell>
+              <TableCell align="right" style={{ color:'white',textAlign:'center'}}>{row.id}</TableCell>
+              <TableCell align="right" style={{ color:'white',textAlign:'center'}}>{row.email}</TableCell>
+              <TableCell align="right"style={{ color:'white',textAlign:'center'}}>{row.projetos}</TableCell>
+              <TableCell align="right"style={{ color:'white',textAlign:'center'}}>{row.assinaturas}</TableCell>
+              <TableCell align="right"style={{ color:'white',textAlign:'center'}}>{row.pais}</TableCell>
+              <TableCell align="right"style={{ color:'white',textAlign:'center', borderRadius:'0,10px,10px,0'}}>{row.lastLogin}</TableCell>
             </TableRow>
           ))}
         </TableBody>
